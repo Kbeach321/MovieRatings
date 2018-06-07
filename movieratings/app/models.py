e3e7eeccc33376c9517be6af58831bfa836d9bca
Kbeach321 <iamkevinbeach@gmail.com>
2018-06-07 14:05:15 -0400

    models set

--- movieratings/app/models.py
+++ movieratings/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+############### Reviewer Class ####################
+class Reviewer(models.Model):
+    age = models.CharField(max_length=3)
+    occupation = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=12)
+
+################ Movie Class ####################
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    imdb_link = models.URLField(max_length=500)
+    director = models.CharField(max_length=100)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+
+############ MovieReview Class #####################
+class MovieReview(models.Model):
+    stars = models.IntegerField()
+
+        ######### ForeignKeys #########
+    movie_review = models.ForeignKey(Reviewer, null=True, on_delete=models.SET_NULL)
+    movie = models.ForeignKey(Movie, null=True, on_delete=models.SET_NULL)
